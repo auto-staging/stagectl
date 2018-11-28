427e92bfe0f8b147f5ff77a257dd4940ec58d0d4
Jan Ritter <jan-ritter@outlook.com>
2018-11-28 17:12:49 +0100

    refactor: replaced custom help message with default one

--- cmd/get.go
+++ cmd/get.go
@@ -30,14 +30,9 @@ import (
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Get information about a specific resource",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Get the available subcommands / resources with 'stagectl get -h'")
+		fmt.Println()
+		cmd.Help()
 	},
 }
 
--- cmd/update.go
+++ cmd/update.go
@@ -26,25 +26,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a new resource",
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update an existing resource",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println()
 		cmd.Help()
 	},
 }
 
-var addEnvironmentCmd = &cobra.Command{
-	Use:   "repository",
-	Short: "Add a repository",
-	Run:   addEnvironmentCmdFunc,
-}
-
 func init() {
-	rootCmd.AddCommand(addCmd)
-	addCmd.AddCommand(addEnvironmentCmd)
+	rootCmd.AddCommand(updateCmd)
 
-	addEnvironmentCmd.Flags().StringP("input-file", "i", ".auto-staging.json", "Filename of the repository definition")
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// updateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// updateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
